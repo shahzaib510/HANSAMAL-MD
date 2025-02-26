f3316cf86d0f67357472545ad13b40b8c0e2e0ea
shahzaib510 <114249604+shahzaib510@users.noreply.github.com>
2025-02-26 22:14:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=oowgCRAL#gZCwrUwX1KdRaRCDEGNxRzunhM0UBaPWwKffhP3HyVw' : process.env.SESSION_ID,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
